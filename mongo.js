e169f471ec965b5439863b45f3066ef00d2b7e22
job <salhiayoub2004@gmail.com>
2024-02-23 22:47:11 +0100

    added two more test + modified the logger + modified mongo(security)

--- mongo.js
+++ mongo.js
@@ -1,14 +1,16 @@
+require("dotenv").config();
+
 const mongoose = require("mongoose");
 
-if (process.argv.length < 3) {
-  console.log("give password as argument");
-  process.exit(1);
-}
+// if (process.argv.length < 3) {
+//   console.log("give password as argument");
+//   process.exit(1);
+// }
 
-const password = process.argv[2];
-// const password = "OE0mq2vFuTznkCly"
 
-const url = `mongodb+srv://jobsal:${password}@noteapp.ksc90sz.mongodb.net/noteApp?retryWrites=true&w=majority`;
+
+
+const url = process.env.TEST_MONGODB_URI;
 
 mongoose.set("strictQuery", false);
 
@@ -29,19 +31,19 @@ noteSchema.set("toJSON", {
 
 const Note = mongoose.model("Note", noteSchema);
 
-// const note = new Note({
-//   content: "GET and POST are the most important methods of HTTP protocol",
-//     important: true
-// })
+const note = new Note({
+  content: "HTML is easy",
+  important: true
+})
 
-// note.save().then(result => {
-//   console.log('note saved!')
-//   mongoose.connection.close()
-// })
+note.save().then(result => {
+  console.log('note saved!')
+  mongoose.connection.close()
+})
 
-Note.find({ important: true }).then((result) => {
-  result.forEach((note) => {
-    console.log(note);
-  });
-  mongoose.connection.close();
-});
+// Note.find({ important: true }).then((result) => {
+//   result.forEach((note) => {
+//     console.log(note);
+//   });
+//   mongoose.connection.close();
+// });
--- tests/note_api.test.js
+++ tests/note_api.test.js
@@ -1,4 +1,4 @@
-const { test, after } = require('node:test')
+const { test, after,assert } = require('node:test')
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
@@ -12,6 +12,21 @@ test('notes are returned as json', async () => {
     .expect('Content-Type', /application\/json/)
 })
 
+test('there are two notes', async () => {
+  const response = await api.get('/api/notes')
+
+  // execution gets here only after the HTTP request is complete
+  // the result of HTTP request is saved in variable response
+  assert.strictEqual(response.body.length, 2)
+})
+
+test('the first note is about HTTP methods', async () => {
+  const response = await api.get('/api/notes')
+
+  const contents = response.body.map(e => e.content)
+  // is the parameter truthy
+  assert(contents.includes('HTML is easy'))
+})
 after(async () => {
   await mongoose.connection.close()
 })
