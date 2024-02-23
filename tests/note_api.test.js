10f7fa1041e1cfdc657ad74d01a0b39180b2ebd7
job <salhiayoub2004@gmail.com>
2024-02-23 23:29:12 +0100

    adding only for the wanted tests

--- tests/note_api.test.js
+++ tests/note_api.test.js
@@ -20,20 +20,19 @@ beforeEach(async () => {
   await noteObject.save();
 });
 
-test("notes are returned as json", async () => {
+test.only('notes are returned as json', async () => {
   await api
-    .get("/api/notes")
+    .get('/api/notes')
     .expect(200)
-    .expect("Content-Type", /application\/json/);
-});
+    .expect('Content-Type', /application\/json/)
+})
 
-test("there are two notes", async () => {
-  const response = await api.get("/api/notes");
+test.only('there are two notes', async () => {
+  const response = await api.get('/api/notes')
 
-  // execution gets here only after the HTTP request is complete
-  // the result of HTTP request is saved in variable response
-  assert.strictEqual(response.body.length, 2);
-});
+  assert.strictEqual(response.body.length, 2)
+})
+
 
 test("the first note is about HTTP methods", async () => {
   const response = await api.get("/api/notes");
@@ -42,6 +41,7 @@ test("the first note is about HTTP methods", async () => {
   // is the parameter truthy
   assert(contents.includes("HTML is easy"));
 });
+
 after(async () => {
   await mongoose.connection.close();
 });
