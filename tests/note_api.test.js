a722b6c5ee98bc9c6e90e7161786f418d7fba15c
job <salhiayoub2004@gmail.com>
2024-02-23 23:15:59 +0100

    initializing db before testing

--- tests/note_api.test.js
+++ tests/note_api.test.js
@@ -1,33 +1,47 @@
-const { test, after } = require('node:test')
-const assert = require('node:assert')
-const mongoose = require('mongoose')
-const supertest = require('supertest')
-const app = require('../app')
+const { test, after, beforeEach } = require("node:test");
+const Note = require("../models/note");
+const assert = require("node:assert");
+const mongoose = require("mongoose");
+const supertest = require("supertest");
+const app = require("../app");
 
-const api = supertest(app)
+const api = supertest(app);
 
-test('notes are returned as json', async () => {
+const initialNotes = [
+  { content: "HTML is easy", important: false },
+  { content: "Browser can execute only JavaScript", important: true },
+];
+
+beforeEach(async () => {
+  await Note.deleteMany({});
+  let noteObject = new Note(initialNotes[0]);
+  await noteObject.save();
+  noteObject = new Note(initialNotes[1]);
+  await noteObject.save();
+});
+
+test("notes are returned as json", async () => {
   await api
-    .get('/api/notes')
+    .get("/api/notes")
     .expect(200)
-    .expect('Content-Type', /application\/json/)
-})
+    .expect("Content-Type", /application\/json/);
+});
 
-test('there are two notes', async () => {
-  const response = await api.get('/api/notes')
+test("there are two notes", async () => {
+  const response = await api.get("/api/notes");
 
   // execution gets here only after the HTTP request is complete
   // the result of HTTP request is saved in variable response
-  assert.strictEqual(response.body.length, 2)
-})
+  assert.strictEqual(response.body.length, 2);
+});
 
-test('the first note is about HTTP methods', async () => {
-  const response = await api.get('/api/notes')
+test("the first note is about HTTP methods", async () => {
+  const response = await api.get("/api/notes");
 
-  const contents = response.body.map(e => e.content)
+  const contents = response.body.map((e) => e.content);
   // is the parameter truthy
-  assert(contents.includes('HTML is easy'))
-})
+  assert(contents.includes("HTML is easy"));
+});
 after(async () => {
-  await mongoose.connection.close()
-})
+  await mongoose.connection.close();
+});
