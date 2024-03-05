a22c1ab565b20d356506c933644f59b3538c16fa
job <salhiayoub2004@gmail.com>
2024-03-05 16:18:39 +0100

    populate

--- controllers/notes.js
+++ controllers/notes.js
@@ -3,21 +3,9 @@ const Note = require("../models/note");
 const User = require("../models/user");
 
 notesRouter.get("/", async (request, response) => {
-  const notes = await Note.find({});
+  const notes = await Note.find({}).populate('user', { username: 1, name: 1 });
   response.json(notes);
 });
-
-// notesRouter.post('/', async (request, response) => {
-//   const body = request.body
-
-//   const note = new Note({
-//     content: body.content,
-//     important: body.important || false,
-//   })
-
-//   const savedNote = await note.save()
-//   response.status(201).json(savedNote)
-// })
 
 notesRouter.post("/", async (request, response) => {
   const body = request.body;
@@ -42,7 +30,6 @@ notesRouter.post("/", async (request, response) => {
 notesRouter.get("/:id", async (request, response) => {
   const note = await Note.findById(request.params.id);
   if (note) {
-    console.log(typeof note.id);
     response.json(note);
   } else {
     response.status(404).end();
